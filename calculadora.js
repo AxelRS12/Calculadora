3732e0346f99141b4555e614629b1af5b921c914
AxelRS12 <72873470+AxelRS12@users.noreply.github.com>
2023-02-18 16:45:05 -0300

    Update Calculadora.js
    Se arreglo según las especificaciones solicitadas

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,9 @@
 alert("Bienvenido a la Calculadora");
-alert("Pasos a seguir de la calculadora:\n A) Ingrese el primer número de la operación \n B) Luego elija la operación que desea realizar Ingresando el numero de referencia \n 1- Sumar \n 2- Restar \n 3- Dividir \n 4- Multiplicar \n 5- Resto \n Luego elija el segundo número de la operación.");
+alert("Pasos a seguir de la calculadora:\n A) Ingrese el primer número de la operación \n B) Luego elija la operación que desea realizar Ingresando el numero de referencia \n 1- Sumar \n 2- Restar \n 3- Dividir \n 4- Multiplicar  \n Luego elija el segundo número de la operación.");
 
 let numero1 = parseInt(prompt("Ingrese el primer número de la operación: ", 0));
 
-let operacion = parseInt(prompt("Seleccione la operación que desea realizar: Ingresando el numero de referencia \n 1- Sumar \n 2- Restar \n 3- Dividir \n 4- Multiplicar \n 5- Resto"));
+let operacion = parseInt(prompt("Seleccione la operación que desea realizar: Ingresando el numero de referencia \n 1- Sumar \n 2- Restar \n 3- Dividir \n 4- Multiplicar \n "));
 
 let numero2 = parseInt(prompt("Ingrese el segundo número de la operación: ", 0));
 
@@ -13,7 +13,7 @@ while (isNaN(numero1) || isNaN(numero2) || isNaN(operacion)) {
 
     numero1 = parseInt(prompt("Ingrese el primer número de la operación: ", 0));
 
-    operacion = parseInt(prompt("Seleccione la operación que desea realizar: Ingresando el numero de referencia \n 1- Sumar \n 2- Restar \n 3- Dividir \n 4- Multiplicar \n 5- Resto"));
+    operacion = parseInt(prompt("Seleccione la operación que desea realizar: Ingresando el numero de referencia \n 1- Sumar \n 2- Restar \n 3- Dividir \n 4- Multiplicar"));
 
     numero2 = parseInt(prompt("Ingrese el segundo número de la operación: ", 0));
 
@@ -21,37 +21,52 @@ while (isNaN(numero1) || isNaN(numero2) || isNaN(operacion)) {
 }
 let resultado = 0;
 
+function sumarNumeros(n1, n2) {
+    resultado = (parseInt(n1) + parseInt(n2));
+    return resultado;
+}
+
+function restarNumeros(n1, n2) {
+    resultado = (parseInt(n1) - parseInt(n2));
+    return resultado;
+}
+
+function multiplicarNumeros(n1, n2) {
+    resultado = (parseInt(n1) * parseInt(n2));
+    return resultado;
+}
+
+function dividirNumeros(n1, n2) {
+    resultado = (parseInt(n1) / parseInt(n2));
+    return resultado;
+}
+
+
 switch (operacion) {
     case 1:
-        resultado = numero1 + numero2;
+        resultado = sumarNumeros(numero1, numero2);
         alert(`El resultado es: ${resultado}`);
-        document.write(`${numero1} + ${numero2} = ${resultado}`);
         break;
     case 2:
-        resultado = numero1 - numero2;
-        alert(`El resultado es: ${resultado}`);
-        document.write(`${numero1} - ${numero2} = ${resultado}`);
+        resultado = restarNumeros(numero1, numero2);
+        alert(`El resultado es: ${resultado}`);            
         break;  
     case 3: 
-        resultado = numero1 / numero2;
         if (numero2 === 0) {
             alert("No se puede Dividir por 0");
             break;
         }
-        alert(`El resultado es: ${resultado}`);
-        document.write(`${numero1} / ${numero2} = ${resultado}`);
-        break;   
+        resultado = dividirNumeros(numero1, numero2);            
+        alert(`El resultado es: ${resultado}`);              
+        break;
     case 4: 
-        resultado = numero1 * numero2;
-        alert(`El resultado es: ${resultado}`);
-        document.write(`${numero1} x ${numero2} = ${resultado}`);
-        break;
-    case 5:
-        resultado = numero1 % numero2;
-        alert(`El resultado es: ${resultado}`);
-        document.write(`${numero1} % ${numero2} = ${resultado}`);
-        break;
+        resultado = multiplicarNumeros(numero1, numero2) ;
+        alert(`El resultado es: ${resultado}`);            
+        break;  
+         
     default:
         alert("Opción seleccionada inválida");
         break;
 }
+
+
